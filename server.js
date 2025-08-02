0fc6465527aebb61714cf449e2b2119524ea6ccc
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-08-02 21:48:13 +0200

    file reading handling added, start server added

--- server.js
+++ server.js
@@ -2,13 +2,25 @@
  * Exercise 3: Create an HTTP web server
  */
 
-const http = require('http');
+import { createServer } from "http";
+import { readFile } from "fs/promises";
 
 //create a server
-let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+let server = createServer(async function (req, res) {
+  try {
+    const html = await readFile("index.html");
+    res.statusCode = 200;
+    res.setHeader("Content-type", "text/html");
+    res.write(html); // Sends a response back to the client
+    res.end(); // Ends the response
+  } catch (err) {
+    console.log("File reading error ", err.message);
+    res.statusCode = 500;
+    res.setHeader("Content-type", "text/plain");
+    res.write("Internal Server Error");
+    res.end();
+  }
 });
 
 server.listen(3000); // The server starts to listen on port 3000
+console.log("Server running at http://localhost:3000/");
